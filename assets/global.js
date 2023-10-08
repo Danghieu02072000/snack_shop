6f23ec889d80a7a8da2f5b14a8ad834ae8073a8f
Dang Hieu <danghieutieumai@gmail.com>
2023-10-08 08:22:49 +0700

    style header page

--- assets/global.js
+++ assets/global.js
@@ -1,1 +1,41 @@
-console.log('hello World');
+const LOGO = document.querySelector('#logo');
+const MENU_MOBIE = document.querySelector('#menu_mobie');
+const CLOSE = document.querySelector('#close');
+const OVERLAY = document.querySelector('#overlay');
+
+LOGO.onclick = function () {
+  MENU_MOBIE.classList.toggle('show');
+  OVERLAY.classList.toggle('show1');
+};
+CLOSE.onclick = function () {
+  MENU_MOBIE.classList.toggle('show');
+  OVERLAY.classList.toggle('show1');
+};
+OVERLAY.onclick = function () {
+  MENU_MOBIE.classList.toggle('show');
+  OVERLAY.classList.toggle('show1');
+};
+$('.customeContainer').owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  autoHeight: true,
+  items: window.number_columns,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    500: {
+      items: 2,
+    },
+    768: {
+      item: 3,
+    },
+    1024: {
+      items: 4,
+    },
+  },
+});
+AOS.init({
+  duration: 300,
+});
